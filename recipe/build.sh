cd2570bea514a741d49fb3ab009cdca785dfb3a3
Nishidha Panpaliya <npanpa23@in.ibm.com>
2021-07-20 13:26:10 +0000

    Some more changes for cuda support and tensorrt enablement

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,8 +17,8 @@
 
 set -ex
 
-rm -r cmake/external/onnx cmake/external/eigen
-mv onnx eigen cmake/external/
+rm -r cmake/external/onnx cmake/external/eigen cmake/external/cub cmake/external/onnx-tensorrt
+mv onnx eigen cub onnx-tensorrt cmake/external/
 
 pushd cmake/external/SafeInt/safeint
 ln -s $PREFIX/include/SafeInt.hpp
@@ -28,6 +28,7 @@ pushd cmake/external/json
 ln -s $PREFIX/include single_include
 popd
 
+
 # Needs eigen 3.4
 # rm -rf cmake/external/eigen
 # pushd cmake/external
@@ -35,10 +36,18 @@ popd
 # popd
 
 CUDA_ARGS=""
+CMAKE_CUDA_EXTRA_DEFINES=""
 if [[ $build_type == "cuda" ]]
 then
   export CUDNN_HOME=$PREFIX
-  CUDA_ARGS=" --use_cuda --cmake_extra_defines CMAKE_CUDA_COMPILER=${CUDA_HOME}/bin/nvcc CMAKE_CUDA_HOST_COMPILER=${CXX}"
+  CUDA_ARGS=" --use_cuda "
+  CMAKE_CUDA_EXTRA_DEFINES="CMAKE_CUDA_COMPILER=${CUDA_HOME}/bin/nvcc CMAKE_CUDA_HOST_COMPILER=${CXX}"
+fi
+
+TRT_FLAG=""
+if [[ $PY_VER > 3.8 ]];
+then
+  TRT_FLAG=" --use_tensorrt --tensorrt_home $PREFIX"
 fi
 
 export CUDACXX=$CUDA_HOME/bin/nvcc
@@ -48,7 +57,8 @@ python tools/ci_build/build.py \
     --build_dir build-ci \
     --use_full_protobuf \
     ${CUDA_ARGS} \
-    --cmake_extra_defines Protobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc Protobuf_INCLUDE_DIR=$PREFIX/include "onnxruntime_PREFER_SYSTEM_LIB=ON" onnxruntime_USE_COREML=OFF CMAKE_PREFIX_PATH=$PREFIX CMAKE_INSTALL_PREFIX=$PREFIX \
+    ${TRT_FLAG} \
+    --cmake_extra_defines ${CMAKE_CUDA_EXTRA_DEFINES} Protobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc Protobuf_INCLUDE_DIR=$PREFIX/include "onnxruntime_PREFER_SYSTEM_LIB=ON" onnxruntime_USE_COREML=OFF CMAKE_PREFIX_PATH=$PREFIX CMAKE_INSTALL_PREFIX=$PREFIX \
     --cmake_generator Ninja \
     --build_wheel \
     --config Release \
